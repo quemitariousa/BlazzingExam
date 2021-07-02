527289d330b56bd18caf5a282360744ded3b7a58
Taha Ghadirian <tahaghad20@gmail.com>
2021-07-02 18:05:04 +0430

    Better docs for login method.

--- WebApps/Server/Controllers/AccountController.cs
+++ WebApps/Server/Controllers/AccountController.cs
@@ -21,7 +21,25 @@ namespace BlazzingExam.WebApps.Server.Controllers
             _userService = userService;
         }
 
+
+        /// <summary>
+        /// Login a user
+        /// </summary>
+        /// <param name="model">Login View Model</param>
+        /// <returns></returns>
+        /// <response code="200">User is successfully loged in.</response>
+        /// <response code="400">Username or password wrong.</response>      
+        /// <remarks>
+        /// Post:
+        ///    {
+        ///        "userName": "Your username",
+        ///        "password": "Your password",
+        ///        "rememberMe": true
+        ///    }
+        /// </remarks>
         [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             var loginUser = await _userService.LoginUserAsync(model);
@@ -30,7 +48,7 @@ namespace BlazzingExam.WebApps.Server.Controllers
 
             await HttpContext.LoginAsync(loginUser, model.RememberMe);
 
-            return Ok(loginUser);
+            return Ok();
         }
     }
 }
